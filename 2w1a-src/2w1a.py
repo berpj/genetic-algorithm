ee4906966c29d49ef2e6f37ccfae1d1ed2998847
Coteazz <arthur.leclerc@epitech.eu>
2015-11-29 19:17:27 +0100

    Base population creation
    - The program creates 20 individual genes , each with 3 for each engine.

--- 2w1a-src/2w1a.py
+++ 2w1a-src/2w1a.py
@@ -1,11 +1,47 @@
-# File created by Thibaut Royer, Epitech school
-# thibaut1.royer@epitech.eu
-# It intends to be an example program for the "Two wheels, one arm" educative project.
-
 import vrep
 import math
 import random
 import time
+
+# Define Individual class
+class Individual:
+    # Constructor
+    def __init__(self, iteration):
+        self.name = "Undividual_" + str(iteration)
+
+        self.genes = []
+        # For each individual, we give him 9 genes, 3 for each motor
+        for i in range(0, 3):
+            awrist = Gene(i, wristHandle, random.randint(0, 300))
+            self.genes.append(awrist)
+
+        for i in range(0, 3):
+            aelbow = Gene(i, elbowHandle, random.randint(0, 300))
+            self.genes.append(aelbow)
+
+        for i in range(0, 3):
+            ashoulder = Gene(i, shoulderHandle, random.randint(0, 300))
+            self.genes.append(ashoulder)
+
+        random.shuffle(self.genes)
+
+    def __repr__(self):
+        return"Individual {name: " + self.name + ", genes: " + str(self.genes) + '}'
+
+
+
+# Define Gene class
+class Gene:
+    # Constructor
+    def __init__(self, iteration, type, action):
+        self.name = "Gene_" + str(iteration)
+        self.type = type
+        self.action = action
+
+    def __repr__(self):
+        return "Individual {name: " + self.name + ', type: ' + self.type + '}'
+
+
 
 print ('Start')
 
@@ -26,74 +62,38 @@ if clientID != -1:
     ret1, wristHandle = vrep.simxGetObjectHandle(clientID, "WristMotor", opmode)
     ret2, elbowHandle = vrep.simxGetObjectHandle(clientID, "ElbowMotor", opmode)
     ret3, shoulderHandle = vrep.simxGetObjectHandle(clientID, "ShoulderMotor", opmode)
+
     ret4, robotHandle = vrep.simxGetObjectHandle(clientID, "2W1A", opmode)
 
     # If handlers are OK, execute three random simulations
     if ret1 == 0 and ret2 == 0 and ret3 == 0:
-        random.seed()
-        for i in range(0, 3):
-            # Start the simulation
-            # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxStartSimulation
-            vrep.simxStartSimulation(clientID, opmode)
-            print "----- Simulation started -----"
 
-            # Start getting the robot position
-            # Unlike other commands, we will use a streaming operating mode
-            # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxGetObjectPosition
-            pret, robotPos = vrep.simxGetObjectPosition(clientID, robotHandle, -1, vrep.simx_opmode_streaming)
-            print "2w1a position: (x = " + str(robotPos[0]) +\
-                  ", y = " + str(robotPos[1]) + ")"
+        # Creating a population of 20 individuals
+        populations = []
 
-            # Start getting the robot orientation
-            # Unlike other commands, we will use a streaming operating mode
-            # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxGetObjectOrientation
-            oret, robotOrient = vrep.simxGetObjectOrientation(clientID, robotHandle, -1, vrep.simx_opmode_streaming)
-            print "2w1a orientation: (x = " + str(robotOrient[0]) + \
-                  ", y = " + str(robotOrient[1]) +\
-                  ", z = " + str(robotOrient[2]) + ")"
+        # Creating each individual
+        for i in range (0, 20):
+            individual = Individual(i)
+            populations.append(individual)
 
-            # Make the robot move randomly five times
-            for j in range(0, 5):
-                # Generating random positions for the motors
-                awrist = random.randint(0, 300)
-                aelbow = random.randint(0, 300)
-                ashoulder = random.randint(0, 300)
+        #print populations
 
-                # The control functions use Radians to determine the target position.
-                # Here, we use maths.radians to convert degrees into radians.
-                # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxSetJointTargetPosition
-                print "Motors target positions: " + str(ashoulder) + " " + str(aelbow) + " " + str(awrist)
-                vrep.simxSetJointTargetPosition(clientID, wristHandle, math.radians(awrist), opmode)
-                vrep.simxSetJointTargetPosition(clientID, elbowHandle, math.radians(aelbow), opmode)
-                vrep.simxSetJointTargetPosition(clientID, shoulderHandle, math.radians(ashoulder), opmode)
+        for individual in populations:
+            for gene in individual.genes:
+                print "----- Simulation started -----"
+                vrep.simxStartSimulation(clientID, opmode)
+
+                vrep.simxSetJointTargetPosition(clientID, gene.type, math.radians(gene.action), opmode)
+                vrep.simxSetJointTargetPosition(clientID, gene.type, math.radians(gene.action), opmode)
+                vrep.simxSetJointTargetPosition(clientID, gene.type, math.radians(gene.action), opmode)
 
                 # Wait in order to let the motors finish their movements
                 # Tip: there must be a more efficient way to do it...
                 time.sleep(5)
 
-                # Get the motors effective positions after the movement sequence
-                # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxGetJointPosition
-                pwrist = vrep.simxGetJointPosition(clientID, wristHandle, opmode)
-                pelbow = vrep.simxGetJointPosition(clientID, elbowHandle, opmode)
-                pshoulder = vrep.simxGetJointPosition(clientID, shoulderHandle, opmode)
-                print "Motors reached positions: " + str(ashoulder) + " " + str(aelbow) + " " + str(awrist)
-
-                # Get the robot position after the movement sequence
-                pret, robotPos = vrep.simxGetObjectPosition(clientID, robotHandle, -1, vrep.simx_opmode_buffer)
-                print "2w1a position: (x = " + str(robotPos[0]) +\
-                      ", y = " + str(robotPos[1]) + ")"
-
-                # Get the robot orientation after the movement sequence
-                oret, robotOrient = vrep.simxGetObjectOrientation(clientID, robotHandle, -1, vrep.simx_opmode_buffer)
-                print "2w1a orientation: (x = " + str(robotOrient[0]) +\
-                      ", y = " + str(robotOrient[1]) +\
-                      ", z = " + str(robotOrient[2]) + ")"
-
-            # End the simulation, wait to be sure V-REP had the time to stop it entirely
-            # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxStopSimulation
-            vrep.simxStopSimulation(clientID, opmode)
-            time.sleep(1)
-            print "----- Simulation ended -----"
+                vrep.simxStopSimulation(clientID, opmode)
+                time.sleep(1)
+                print "----- Simulation ended -----"
 
     # Close the connection to V-REP remote server
     # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxFinish
