91b659d7f8a80f8a1eb8c8a7255d04fcc159041f
Nathan Pichon <npichon.walchshofer@gmail.com>
2015-11-30 23:16:10 +0100

    Ajout du score (distant/temps) et de la selection

--- 2w1a-src/2w1a.py
+++ 2w1a-src/2w1a.py
@@ -4,7 +4,7 @@ import random
 import time
 
 POPIND = 20 # Nbr d'individu par population
-BESTIND = POPIND / 4 # Nbr d'individu garde a la fin des selections
+BESTIND = int(POPIND / 4) # Nbr d'individu garde a la fin des selections
 NBGENE = 9 # Multiple de trois
 MUTATE = 50 # Chiffre compris entre 0 et 100 nombre d'individus concerne par la mutation dans une population en pourcentage
 GMUTATE = 30 # Chiffre compris entre 0 et 100 nombre de genes concerne par la mutation dans un individu en pourcentage
@@ -16,16 +16,17 @@ MAXMOTOR = 300
 class Individual:
     # Constructor
     def __init__(self, iteration):
-        self.name = "Undividual_" + str(iteration)
+        self.name = "Individual_" + str(iteration)
         self.generation = 0
         self.maxDistance = 0
+        self.score = 0
 
         self.genes = []
 
         random.shuffle(self.genes)
 
     def __repr__(self):
-        return"Individual {name: " + self.name + ", genes: " + str(self.genes) + '}'
+        return"Individual {name: " + self.name + ", score: " + str(self.score) + '}'
 
     def setGene(self, type, gene):
         for i in range(0, NBGENE):
@@ -38,6 +39,17 @@ class Individual:
             self.genes.append(Gene(len(self.genes), type, random.randint(MINMOTOR, MAXMOTOR)))
         random.shuffle(self.genes)
 
+    def setDistance(self, distance):
+        self.maxDistance = distance
+    
+    def getDistance(self):
+        return self.maxDistance
+
+    def setScore(self, score):
+        self.score = score * 10000
+    
+    def getScore(self):
+        return self.score
 
 
 # Define Gene class
@@ -49,7 +61,7 @@ class Gene:
         self.action = action
 
     def __repr__(self):
-        return "Individual {name: " + self.name + ', type: ' + self.type + '}'
+        return "Gene {name: " + self.name + ', type: ' + str(self.type) + '}'
 
 
 
@@ -93,11 +105,13 @@ if clientID != -1:
 
         #print populations
         populations = population1 + population2
+        scoreTotal = 0
 
         for individual in populations:
 
-            print "----- Simulation started -----"
+            print "----- Evaluation started -----"
             vrep.simxStartSimulation(clientID, opmode)
+            theTime = time.time()
             pret, robotPos = vrep.simxGetObjectPosition(clientID, robotHandle, -1, vrep.simx_opmode_streaming)
             print "2w1a position: (x = " + str(robotPos[0]) + ", y = " + str(robotPos[1]) + ")"
 
@@ -108,15 +122,35 @@ if clientID != -1:
                 print "Motor " + str(gene.type) + " reached position: " + str(gene.action) + " degree"
                 # Wait in order to let the motors finish their movements
                 # Tip: there must be a more efficient way to do it...
+                # See simxGetPingTime maybe ?
+                # or simxGetLastCmdTime
                 time.sleep(3)
 
             pret, robotPosEnd = vrep.simxGetObjectPosition(clientID, robotHandle, -1, vrep.simx_opmode_streaming)
             print "2w1a position: (x = " + str(robotPosEnd[0]) + ", y = " + str(robotPosEnd[1]) + ")"
-            print "Distance parcourue: " + str(math.sqrt(math.pow(robotPosEnd[0] - robotPos[0],2) + math.pow(robotPosEnd[1] - robotPos[1],2)))
+            individual.setDistance(math.sqrt(math.pow(robotPosEnd[0] - robotPos[0],2) + math.pow(robotPosEnd[1] - robotPos[1],2)))
+            theTime = time.time() - theTime
+            individual.setScore(individual.getDistance() / theTime)
+            print "Distance parcourue: " + str(individual.getDistance())
+            print "Score obtenu: " + str(individual.getScore())
+            scoreTotal = scoreTotal + individual.getScore()
             vrep.simxStopSimulation(clientID, opmode)
-            print "----- Simulation ended -----"
+            print "----- Evaluation ended -----"
             time.sleep(2)
-
+        print "----- Selection par roulette -----"
+        selection = []
+        for i in range(0, BESTIND):
+            random.shuffle(populations)
+            scoreToReach = random.randint(0, int(scoreTotal))
+            tmpScore = 0
+            for individu in populations:
+                tmpScore = tmpScore + individu.getScore()
+                if (tmpScore >= scoreToReach):
+                    selection.append(individu)
+                    scoreTotal = scoreTotal - individu.getScore()
+                    populations.remove(individu)
+                    break
+        print "----- Fin de la selection -----"
     # Close the connection to V-REP remote server
     # http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm#simxFinish
     vrep.simxFinish(clientID)
